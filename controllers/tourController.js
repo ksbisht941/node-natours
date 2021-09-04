945b4ed500ba841799cb503db89d990f8f0bcf4a
Kevin <kevin@Kevins-MacBook-Air.local>
2021-09-04 10:37:44 +0530

    implement basic and advance way to filter in api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -39,7 +39,35 @@ exports.getAllTours = async (req, res) => {
   // console.log(req.requestTime);
 
   try {
-    const tours = await Tour.find();
+    // #1 method
+    // const tours = await Tour.find({
+    //   duration: 5,
+    //   difficulty: 'easy'
+    // });
+
+    // #2 method
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // BUILD QUERY
+    // 1) Basic Filtering
+    const queryObj = { ...req.query };
+
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // 2) Advance Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    console.log('queryStr', JSON.parse(queryStr));
+
+    const query = Tour.find(JSON.parse(queryStr));
+
+    // EXECUTE QUERY
+    const tours = await query;
 
     res.status(200).json({
       status: "success",
@@ -99,7 +127,7 @@ exports.getTour = async (req, res) => {
 };
 
 exports.createTour = async (req, res) => {
-  console.log('createTour', req.body);
+  // console.log('createTour', req.body);
   try {
     // #1 Method
     // const newTour = new Tour({})
@@ -150,7 +178,7 @@ exports.updateTour = async (req, res) => {
   try {
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
-      runValidators: true
+      runValidators: true,
     });
 
     res.status(200).json({
@@ -171,20 +199,18 @@ exports.updateTour = async (req, res) => {
 
 exports.deleteTour = async (req, res) => {
   try {
-
     await Tour.findByIdAndDelete(req.params.id);
 
     res.status(200).json({
       status: "success",
       code: "200",
-      message: 'Tour delete successfully 💥'
+      message: "Tour delete successfully 💥",
     });
-  } catch(err) {
+  } catch (err) {
     res.status(404).json({
       status: "failed",
       code: "404",
-      message: 'Unable to delete tour 💥'
+      message: "Unable to delete tour 💥",
     });
   }
-  
 };
